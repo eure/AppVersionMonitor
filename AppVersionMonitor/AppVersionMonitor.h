da468f7a992d6f4ec2da37ef8fad16f57c16509a
muukii <m@muukii.me>
2016-10-25 12:05:25 +0900

    Revise Project

---
+++ AppVersionMonitor/AppVersionMonitor.h
@@ -0,0 +1,19 @@
+//
+//  AppVersionMonitor.h
+//  AppVersionMonitor
+//
+//  Created by muukii on 2016/10/25.
+//  Copyright © 2016 muukii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppVersionMonitor.
+FOUNDATION_EXPORT double AppVersionMonitorVersionNumber;
+
+//! Project version string for AppVersionMonitor.
+FOUNDATION_EXPORT const unsigned char AppVersionMonitorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppVersionMonitor/PublicHeader.h>
+
+
--- Example/Pods/Target Support Files/AppVersionMonitor/AppVersionMonitor-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_AppVersionMonitor : NSObject
-@end
-@implementation PodsDummy_AppVersionMonitor
-@end
