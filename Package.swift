b6b307a582ff04fbe9d5ede228daf82c219f6e3a
Zach McGaughey <1799888+made2k@users.noreply.github.com>
2020-02-26 09:51:11 -0700

    Updated Package.swift and swift version

--- Package.swift
+++ Package.swift
@@ -1,6 +1,16 @@
-
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
-    name: "AppVersionMonitor"
+    name: "AppVersionMonitor",
+    // platforms: [.iOS("8.0")],
+    products: [
+        .library(name: "AppVersionMonitor", targets: ["AppVersionMonitor"])
+    ],
+    targets: [
+        .target(
+            name: "AppVersionMonitor",
+            path: "Sources"
+        )
+    ]
 )
