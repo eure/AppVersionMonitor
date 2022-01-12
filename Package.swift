bb072ae883493d732846398517d0ab049a7c32e5
Muukii <muukii.app@gmail.com>
2022-01-12 13:07:10 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,6 +1,18 @@
-
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
-    name: "AppVersionMonitor"
+  name: "AppVersionMonitor",
+  platforms: [.iOS(.v12)],
+  products: [
+    .library(name: "AppVersionMonitor", targets: ["AppVersionMonitor"]),
+  ],
+  dependencies: [
+  ],
+  targets: [
+    .target(
+      name: "AppVersionMonitor",
+      exclude: ["Info.plist"]
+    ),
+  ]
 )
